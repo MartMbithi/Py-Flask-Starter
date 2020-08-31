3cc45c53c1189dac7171c356e0b8c55f02ac8ac1
MartMbithi <martdevelopers254@gmail.com>
2020-08-31 13:15:04 +0300

    Regisgration form class

--- app.py
+++ app.py
@@ -22,6 +22,18 @@ def index():
     return render_template('welcome.html')
 
 
+# Registration Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
 
 if __name__ == '__main__':
     app.secret_key='secret123'
