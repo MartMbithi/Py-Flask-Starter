9b1f3e8b142dcce0599f41eadcb0d5745be91e9d
MartMbithi <martdevelopers254@gmail.com>
2020-08-31 13:22:07 +0300

    Registration Backend

--- app.py
+++ app.py
@@ -34,6 +34,34 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
+# User Registration
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
 
 if __name__ == '__main__':
     app.secret_key='secret123'
