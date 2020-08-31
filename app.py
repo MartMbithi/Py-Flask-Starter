1e03a5c5c5f0b2bf839af079ca5aff2d30043c45
MartMbithi <martdevelopers254@gmail.com>
2020-08-31 12:52:39 +0300

    Loaded Database connection

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from functools import wraps
 
 app = Flask(__name__)
 
+# Config MySQL
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'martdevelopers_flask_starter'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
+
 
 
 if __name__ == '__main__':
